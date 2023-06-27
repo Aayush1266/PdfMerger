192c53114217c186c7b33c2268f957f4a4c70c89
Aayush Sethi <108228521+Aayush1266@users.noreply.github.com>
2023-06-27 14:18:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import PyPDF2
+
+pdffiles = ["1.pdf","2.pdf"]
+merger = PyPDF2.PdfMerger()
+for filename in pdffiles:
+    pdffiles= open(filename, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdffiles)
+    merger.append(pdfReader)
+pdffiles.close()
+merger.write('merged.pdf')
+
+
+
+
+
+
+
+
+
